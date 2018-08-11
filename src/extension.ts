a5cfbc9a7c7511e2fb721dfcfdfbcc5770bce719
Julien Lengrand-Lambert <Julien.Lengrand-Lambert@ing.nl>
2018-08-11 15:50:35 +0200

    Counting elm files

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,8 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "vscode-elm-signature" is now active!');
     vscode.window.showInformationMessage('Congratulations, your extension "vscode-elm-signature" is now active!');
 
@@ -19,7 +13,6 @@ export function activate(context: vscode.ExtensionContext) {
     context.subscriptions.push(elmSignaturecontroller);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {
 }
 
@@ -33,7 +26,10 @@ class ElmSignatureDisplayer{
     public updateDataFound() {
         this.saveCounter++;
 
-        this._statusBarItem.text = `${this.saveCounter} times saved`;
+        const elmDocuments = vscode.workspace.textDocuments.filter(doc => doc.fileName.endsWith('.elm'));
+        this.elmFilesCounter = elmDocuments.length;
+
+        this._statusBarItem.text = `${this.saveCounter} times saved. ${this.elmFilesCounter} elm files found`;
         this._statusBarItem.show();
 
     }
@@ -53,10 +49,7 @@ class ElmSignatureController {
 
         let subscriptions: vscode.Disposable[] = [];
 
-        //TODO : Change those events
-        vscode.window.onDidChangeTextEditorSelection(this._onEvent, this, subscriptions);
-        vscode.window.onDidChangeActiveTextEditor(this._onEvent, this, subscriptions);
-
+        vscode.workspace.onDidSaveTextDocument(this._onEvent, this, subscriptions);
         this.elmSignatureDisplayer.updateDataFound();
 
         this._disposable = vscode.Disposable.from(...subscriptions);
